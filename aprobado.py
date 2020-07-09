6060649b177070ab73509b574ad0e371b80408fb
Giampierre Huaman Berru <2017210499@untels.edu.pe>
2020-07-09 15:00:15 -0500

    Metodo para saber la aprobacion

--- aprobado.py
+++ aprobado.py
@@ -1,3 +1,10 @@
 # Ejercico que determine si un estudiante esta aprobado o no.
 # Autor :Giampierre Huaman Berru
 
+
+def determinaraprobado(promedio):
+    if promedio>=11:
+        resultado="Aprobado"
+    else:
+        resultado="Desaprobado"
+    return resultado
